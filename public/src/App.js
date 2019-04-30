36bf96b97637a5053a7ec2f17f187a6d7c07f587
AJ Larson <aj.larson@devmounta.in>
2019-04-30 12:00:14 -0600

    updated for QA assignment

--- public/src/App.js
+++ public/src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to My Bookshelf</h2>
         </div>
         <div className="App-intro">
         <form action="submit" onSubmit={this.addBook}>
